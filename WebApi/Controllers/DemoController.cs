e083c691e246a3ba8998ac94ff88037b7965db41
Lingann <lingann.m@gmail.com>
2019-06-28 09:32:32 +0800

    添加项目文件。

---
+++ WebApi/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "嗝"};
+        }
+    }
+}
